f450e1dbc7f31b80fd6ffe666c54cfe0d3ec4f80
fil <filipisee@hotmail.com>
2022-09-12 21:00:41 -0300

    testing arrow functions

--- js-refresh/js-tests.js
+++ js-refresh/js-tests.js
@@ -8,3 +8,12 @@ let myLetName = 'Menu'
 console.log(myLetName) */
 
 // Arrow functions
+/* const myFunction = (name, age) => {
+    console.log('Hello Arrow Function!')
+    console.log(`My name is ${name} and my age is ${age}.`)
+}
+
+myFunction("Fil", 20) */
+const multiply = (num) => num * num
+
+console.log('The square of 4 is ' + multiply(4) + '.')
