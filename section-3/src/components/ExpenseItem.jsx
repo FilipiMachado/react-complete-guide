9657fc650ebbea692602b587991b093d4a0c8337
fil <filipisee@hotmail.com>
2022-09-13 20:39:29 -0300

    styling expense item component

--- section-3/src/components/ExpenseItem.jsx
+++ section-3/src/components/ExpenseItem.jsx
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>March 28th</div>
-      <div>
+    <div className="expense-item">
+      <div>March 28th 2022</div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
