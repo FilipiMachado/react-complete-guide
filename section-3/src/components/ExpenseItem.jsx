89c48220626a80184c5c76b937bf570b297e0a75
fil <filipisee@hotmail.com>
2022-09-13 21:17:24 -0300

    passing data via props

--- section-3/src/components/ExpenseItem.jsx
+++ section-3/src/components/ExpenseItem.jsx
@@ -1,11 +1,13 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">$294.67</div>
       </div>
     </div>
