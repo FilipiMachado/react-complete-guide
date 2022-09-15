22758eb0228ddee2cb0560c42257364e4ec5382a
fil <filipisee@hotmail.com>
2022-09-15 11:36:51 -0300

    working with events

--- react-refresh/src/components/ExpenseItem/ExpenseItem.jsx
+++ react-refresh/src/components/ExpenseItem/ExpenseItem.jsx
@@ -4,6 +4,10 @@ import Card from "../Card/Card"
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log('test')
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +15,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
