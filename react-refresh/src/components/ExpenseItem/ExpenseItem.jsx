058844d219b79305e75314760ab2274f963a67ed
fil <filipisee@hotmail.com>
2022-09-15 12:01:21 -0300

    testing on useState hooks

--- react-refresh/src/components/ExpenseItem/ExpenseItem.jsx
+++ react-refresh/src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
+
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../Card/Card"
 
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const clickHandler = () => {
-    console.log('test')
+  const [newTitle, setNewTitle] = useState(props.title)
+
+  const clickHandler = (e) => {
+    setNewTitle(e.target.value)
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{newTitle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <input type="text"
+             id="new-title"
+             name="new-title"
+             onChange={clickHandler}
+             value={newTitle}/>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
