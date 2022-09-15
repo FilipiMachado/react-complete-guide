e0e475343c7869dfc90d38a61c284bb444a9f7cd
fil <filipisee@hotmail.com>
2022-09-15 14:26:22 -0300

    styling expenseform/newexpense components

---
+++ react-refresh/src/components/ExpenseForm/ExpenseForm.jsx
@@ -0,0 +1,11 @@
+import './ExpenseForm.css'
+
+const ExpenseForm= () => {
+  return (
+    <div>
+      <div>Expense Form</div>
+    </div>
+  )
+}
+
+export default ExpenseForm
---
+++ react-refresh/src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,11 @@
+import './NewExpense.css'
+
+const NewExpense= () => {
+  return (
+    <div>
+      <div>New Expense</div>
+    </div>
+  )
+}
+
+export default NewExpense
